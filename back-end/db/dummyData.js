a40a91fb7c81c487dfab0f94f10423119800f98e
firko-learnin <jackfirkin@gmail.com>
2022-01-18 12:12:13 +0000

    Install dependencies, prep for deployment

---
+++ back-end/db/dummyData.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    id: "abc",
+    roomNumber: 1,
+  },
+  {
+    id: "def",
+    roomNumber: 2,
+  },
+  {
+    id: "ghi",
+    roomNumber: 3,
+  },
+];
+
+const unsolvedProblems = [
+  {
+    roomNumber: 1,
+    title: "Problem 1",
+    text: "Problem description text",
+  },
+  {
+    roomNumber: 2,
+    title: "Problem 2",
+    text: "Problem description text",
+  },
+  {
+    roomNumber: 3,
+    title: "Problem 3",
+    text: "Problem description text",
+  },
+];
+
+const solvedProblems = [
+  {
+    roomNumber: 4,
+    title: "Problem 4",
+    text: "Problem description text",
+  },
+  {
+    roomNumber: 5,
+    title: "Problem 5",
+    text: "Problem description text",
+  },
+  {
+    roomNumber: 6,
+    title: "Problem 6",
+    text: "Problem description text",
+  },
+];
