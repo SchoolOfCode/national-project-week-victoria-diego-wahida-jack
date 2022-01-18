6b3d59c9d1a6638a907ec9dc243d8ff4de4a270d
Victoriam97 <victoriameah@hotmail.com>
2022-01-18 14:28:51 +0000

    Component folders made

---
+++ back-end/front-end/src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"; 
+import BeingSolvedArea from "../BeingSolvedArea";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <BeingSolvedArea />
+    </div>
+  );
+}
+
+export default App;
---
+++ back-end/front-end/src/Components/BeingSolvedArea/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BeingSolvedArea() {
+    return (
+        <div>
+            <h1>Being Solved</h1>
+        </div>
+    )
+}
+
+export default BeingSolvedArea
