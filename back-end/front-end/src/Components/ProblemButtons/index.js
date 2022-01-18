b16cb58b95734819e831fb3537216cfb5038eaad
firko-learnin <jackfirkin@gmail.com>
2022-01-18 16:43:03 +0000

    Add material icons

--- back-end/front-end/src/Components/ProblemButtons/index.js
+++ back-end/front-end/src/Components/ProblemButtons/index.js
@@ -1,16 +1,19 @@
-import React from 'react'
-
-
+import React from "react";
+import SupportIcon from "@mui/icons-material/Support";
+import MeetingRoomOutlinedIcon from "@mui/icons-material/MeetingRoomOutlined";
+import PlusOneOutlinedIcon from "@mui/icons-material/PlusOneOutlined";
+import PeopleIcon from "@mui/icons-material/People";
+import "./styles.css";
 
 function ProblemButtons() {
-    return (
-        <div className="problem-buttons">
-           <button ></button>
-           <button ></button>
-           <button ></button>
-           <button ></button>
-        </div>
-    )
+  return (
+    <div className="problem-buttons">
+      <MeetingRoomOutlinedIcon></MeetingRoomOutlinedIcon>
+      <SupportIcon></SupportIcon>
+      <PlusOneOutlinedIcon></PlusOneOutlinedIcon>
+      <PeopleIcon></PeopleIcon>
+    </div>
+  );
 }
 
-export default ProblemButtons
+export default ProblemButtons;
