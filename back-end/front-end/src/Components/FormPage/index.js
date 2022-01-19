aabf9e18f18be37130f29f95f5a77d74c798a3e6
firko-learnin <jackfirkin@gmail.com>
2022-01-19 18:02:03 +0000

    Convert form to a basic working modal, needs styling

--- back-end/front-end/src/Components/FormPage/index.js
+++ back-end/front-end/src/Components/FormPage/index.js
@@ -1,53 +1,127 @@
-import React, {useState} from 'react'
+import React, {
+  useState,
+  useImperativeHandle,
+  forwardRef,
+  useCallback,
+  useEffect,
+} from "react";
+import { createPortal } from "react-dom";
+import "./styles.css";
+import CancelIcon from "@mui/icons-material/Cancel";
 
-function FormPage() {
-    const formTemplate = {
-        person: "",
-        roomNumber: "",
-        problemTitle: "",
-        steps: "",
-        checkbox: false
+const modalElement = document.getElementById("modal-root");
+
+export function FormPage({ defaultOpened = false }, ref) {
+  const [isOpen, setIsOpen] = useState(defaultOpened);
+  const close = useCallback(() => setIsOpen(false), []);
+
+  const formTemplate = {
+    person: "",
+    roomNumber: "",
+    problemTitle: "",
+    steps: "",
+    checkbox: false,
+  };
+
+  const [formData, setFormData] = useState(formTemplate);
+  const [validForm, setValidForm] = useState(true);
+
+  function handleChange(item, value) {
+    setFormData({ ...formData, [item]: value });
+
+    if (
+      formData.person === "" ||
+      formData.roomNumber === "" ||
+      formData.problemTitle === "" ||
+      formData.steps === "" ||
+      formData.checkbox === false
+    ) {
+      console.log(formData.checkbox);
+      return;
+    } else {
+      setValidForm(false);
     }
+  }
 
-    const [formData,setFormData] = useState(formTemplate)
-    const [validForm, setValidForm] = useState(true)
+  function clearForm() {
+    setFormData(formTemplate);
+    setValidForm(true);
+  }
+  useImperativeHandle(
+    ref,
+    () => ({
+      open: () => setIsOpen(true),
+      close,
+    }),
+    [close]
+  );
 
-    function handleChange(item,value) {
-        setFormData({...formData, [item]:value})
-        
-        if(formData.person === "" || formData.roomNumber === "" || formData.problemTitle=== "" || formData.steps=== "" || formData.checkbox === false) {
-            console.log(formData.checkbox)
-            return;
-        } else {
-            setValidForm(false)
-        }
+  const handleEscape = useCallback(
+    (event) => {
+      if (event.keyCode === 27) close();
+    },
+    [close]
+  );
+  useEffect(() => {
+    if (isOpen) document.addEventListener("keydown", handleEscape, false);
+    return () => {
+      document.removeEventListener("keydown", handleEscape, false);
+    };
+  }, [handleEscape, isOpen]);
 
-    }
-
-    function clearForm() {
-        setFormData(formTemplate)
-        setValidForm(true)
-    }
-
-    return (
-        <section>
+  return createPortal(
+    isOpen ? (
+      <section className="modal">
+        <div className="modal-overlay" onClick={close}></div>
+        <CancelIcon className="modal-close" onClick={close}></CancelIcon>
         <h1>Form Page</h1>
         <label>
-            Have you googled it?
-        <input type="checkbox" checked={formData.checkbox} value={formData.checkbox} onChange={(e)=> handleChange("checkbox", e.target.checked)} ></input>
+          Have you googled it?
+          <input
+            type="checkbox"
+            checked={formData.checkbox}
+            value={formData.checkbox}
+            onChange={(e) => handleChange("checkbox", e.target.checked)}
+          ></input>
         </label>
         <br></br>
-        <input placeholder = "enter your name" onChange={(e)=> handleChange("person", e.target.value)} value={formData.person}></input>
+        <input
+          placeholder="enter your name"
+          onChange={(e) => handleChange("person", e.target.value)}
+          value={formData.person}
+        ></input>
         <br></br>
-        <input placeholder="room #" onChange={(e)=> handleChange("roomNumber", e.target.value)} value={formData.roomNumber}></input>
+        <input
+          placeholder="room #"
+          onChange={(e) => handleChange("roomNumber", e.target.value)}
+          value={formData.roomNumber}
+        ></input>
         <br></br>
-        <input placeholder="problem title" onChange={(e)=> handleChange("problemTitle", e.target.value)} value={formData.problemTitle}></input>
+        <input
+          placeholder="problem title"
+          onChange={(e) => handleChange("problemTitle", e.target.value)}
+          value={formData.problemTitle}
+        ></input>
         <br></br>
-        <input placeholder="steps taken to solve problem" onChange={(e)=> handleChange("steps", e.target.value)} value={formData.steps}></input>
+        <input
+          placeholder="steps taken to solve problem"
+          onChange={(e) => handleChange("steps", e.target.value)}
+          value={formData.steps}
+        ></input>
         <br></br>
-        <button onClick={(e)=>{clearForm(e); clearForm();}} disabled={validForm}>Submit</button>
+        <button
+          onClick={(e) => {
+            clearForm(e);
+            setIsOpen(false);
+          }}
+          disabled={validForm}
+        >
+          Submit
+        </button>
       </section>
-    );
+    ) : null,
+    modalElement
+  );
 }
-
-export default FormPage;
+export default forwardRef(FormPage);
+// export default FormPage;
--- back-end/front-end/src/Components/ToBeSolvedArea/index.js
+++ back-end/front-end/src/Components/ToBeSolvedArea/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import ProblemList from "../ProblemList";
 import "./styles.css";
+import AddIcon from "@mui/icons-material/Add";
+import FormPage from "../FormPage";
 
 function ToBeSolvedArea({ problems }) {
+  const modal = useRef(null);
   return (
     <section className="toBeSolvedArea">
-      <h1>To Be Solved</h1>
+      <div className="header">
+        <h1>To Be Solved</h1>
+        <AddIcon
+          onClick={() => modal.current.open()}
+          color="primary"
+          fontSize="large"
+        ></AddIcon>
+      </div>
       <ProblemList problems={problems}></ProblemList>
+      <FormPage ref={modal}></FormPage>
     </section>
   );
 }
