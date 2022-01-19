da0443023bf324b594d39f2c3cc337a68543ac02
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2022-01-19 16:15:35 +0000

    Problem Input

--- back-end/front-end/src/Components/FormPage/index.js
+++ back-end/front-end/src/Components/FormPage/index.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function FormPage() {
-    
+    const formTemplate = {
+        person: "",
+        roomNumber: "",
+        problemTitle: "",
+        steps: "",
+        checkbox: false
+    }
+
+    const [formData,setFormData] = useState(formTemplate)
+    const [validForm, setValidForm] = useState(true)
+
+    function handleChange(item,value) {
+        setFormData({...formData, [item]:value})
+        
+        if(formData.person === "" || formData.roomNumber === "" || formData.problemTitle=== "" || formData.steps=== "" || formData.checkbox === false) {
+            console.log(formData.checkbox)
+            return;
+        } else {
+            setValidForm(false)
+        }
+
+    }
+
+    function clearForm() {
+        setFormData(formTemplate)
+        setValidForm(true)
+    }
+
     return (
         <section>
         <h1>Form Page</h1>
-        <input placeholder="room #"></input>
-        <br></br>
-        <input placeholder="problem title"></input>
-        <br></br>
-        <input placeholder="steps taken to solve problem"></input>
-        <br></br>
         <label>
             Have you googled it?
-        <input type="checkbox"></input>
+        <input type="checkbox" checked={formData.checkbox} value={formData.checkbox} onChange={(e)=> handleChange("checkbox", e.target.checked)} ></input>
         </label>
         <br></br>
-        <button>Submit</button>
+        <input placeholder = "enter your name" onChange={(e)=> handleChange("person", e.target.value)} value={formData.person}></input>
+        <br></br>
+        <input placeholder="room #" onChange={(e)=> handleChange("roomNumber", e.target.value)} value={formData.roomNumber}></input>
+        <br></br>
+        <input placeholder="problem title" onChange={(e)=> handleChange("problemTitle", e.target.value)} value={formData.problemTitle}></input>
+        <br></br>
+        <input placeholder="steps taken to solve problem" onChange={(e)=> handleChange("steps", e.target.value)} value={formData.steps}></input>
+        <br></br>
+        <button onClick={(e)=>{clearForm(e); clearForm();}} disabled={validForm}>Submit</button>
       </section>
     );
 }
