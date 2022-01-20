bad2303024f4ef0017f790c318409731d10aaf0d
firko-learnin <jackfirkin@gmail.com>
2022-01-20 19:34:09 +0000

    Add complete problem button, post to solved problems and delete from unsolved problems

--- back-end/front-end/src/Components/ProblemButtons/index.js
+++ back-end/front-end/src/Components/ProblemButtons/index.js
@@ -3,7 +3,10 @@ import SupportIcon from "@mui/icons-material/Support";
 import MeetingRoomOutlinedIcon from "@mui/icons-material/MeetingRoomOutlined";
 import PlusOneOutlinedIcon from "@mui/icons-material/PlusOneOutlined";
 import PeopleIcon from "@mui/icons-material/People";
+import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 import "./styles.css";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function ProblemButtons({ problem, handleClick }) {
   const [count, setCount] = useState(0);
@@ -13,19 +16,53 @@ function ProblemButtons({ problem, handleClick }) {
     console.log("count");
   }
 
+  async function completeProblem(problem) {
+    //Add the relevant data to a new object to POST
+    const newSolvedProblem = {
+      roomnumber: problem.roomnumber,
+      title: problem.title,
+      text: problem.text,
+    };
+    console.log(newSolvedProblem);
+    // setProblems([...problems, entry])
+    //Post the data to the solved problems table
+    const res = await fetch(`${API_URL}/solvedproblems/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newSolvedProblem),
+    });
+    console.log(res);
+    //Delete the entry from the old table
+    const deleteRes = await fetch(`${API_URL}/unsolvedproblems/${problem.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(deleteRes);
+    //Refresh the page to update
+    window.location.reload();
+  }
+
   if (problem.beingsolved === true) {
     return (
       <div className="problem-buttons">
         <button id={problem.id} onClick={() => handleClick(problem.id)}>
-        <SupportIcon></SupportIcon>
+          <SupportIcon></SupportIcon>
         </button>
         <PlusOneOutlinedIcon
           onClick={IncrementCount}
           id={problem.id}
         ></PlusOneOutlinedIcon>
-        <PeopleIcon></PeopleIcon>
-
-        <p>{count}</p>
+        <div className="counter">
+          <PeopleIcon></PeopleIcon>
+          <p>{count}</p>
+        </div>
+        <button id={problem.id} onClick={() => completeProblem(problem)}>
+          <DoneOutlineIcon></DoneOutlineIcon>
+        </button>
       </div>
     );
   } else if (problem.beingsolved === false) {
@@ -33,7 +70,7 @@ function ProblemButtons({ problem, handleClick }) {
       <div className="problem-buttons">
         <MeetingRoomOutlinedIcon></MeetingRoomOutlinedIcon>
         <button id={problem.id} onClick={() => handleClick(problem.id)}>
-        <SupportIcon></SupportIcon>
+          <SupportIcon></SupportIcon>
         </button>
         <PlusOneOutlinedIcon
           onClick={IncrementCount}
