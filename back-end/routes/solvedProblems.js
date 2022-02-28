018328fe967b48f247da842d1c90ec8bc92386bf
firko-learnin <jackfirkin@gmail.com>
2022-02-28 21:00:18 +0000

    Add delete solved problems route

--- back-end/routes/solvedProblems.js
+++ back-end/routes/solvedProblems.js
@@ -1,31 +1,43 @@
 import express from "express";
 const router = express.Router();
-import { getAllSolvedProblems, getSolvedProblemsByID, createNewSolvedProblem, UpdateSolvedProblemByID, DeleteSolvedProblemByID} from "../models/users.js";
+import {
+  getAllSolvedProblems,
+  getSolvedProblemsByID,
+  createNewSolvedProblem,
+  UpdateSolvedProblemByID,
+  DeleteSolvedProblemByID,
+} from "../models/users.js";
 
 //Solved Problems
-router.get("/", async function (req,res) {
-    res.json({
-        "success": true,
-        "payload": await getAllSolvedProblems()
-    })
-  })
-  
-  
-  router.get("/:id", async function (req,res){
-    const id = req.params.id;
-    res.json({
-        "success": true,
-        "payload": await getSolvedProblemsByID(id)    
-    });
-  })
-  
-  router.post("/", async function (req,res){
-    const problem = req.body
-    res.json({
-        "success": true,
-        "payload": await createNewSolvedProblem(problem)
-    })
-  })
-  
+router.get("/", async function (req, res) {
+  res.json({
+    success: true,
+    payload: await getAllSolvedProblems(),
+  });
+});
 
-  export default router;
+router.get("/:id", async function (req, res) {
+  const id = req.params.id;
+  res.json({
+    success: true,
+    payload: await getSolvedProblemsByID(id),
+  });
+});
+
+router.post("/", async function (req, res) {
+  const problem = req.body;
+  res.json({
+    success: true,
+    payload: await createNewSolvedProblem(problem),
+  });
+});
+
+router.delete("/:id", async function (req, res) {
+  const id = req.params.id;
+  res.json({
+    success: true,
+    payload: await DeleteSolvedProblemByID(id),
+  });
+});
+
+export default router;
